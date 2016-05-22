d751bd5a2453f6cbefb71560bd9ec599f77617cb
yaacovCR <yaacovCR@gmail.com>
2016-05-21 22:55:18 -0400

    [fix] server now takes care of case insensitive unique value filtering

--- src/app/components/search/addStringCriterionDialog.js
+++ src/app/components/search/addStringCriterionDialog.js
@@ -11,10 +11,8 @@
     vm.cancel = cancel;
     vm.submit = submit;
     
-    vm.options = ServerStudy.query({ distinct: vm.property }).$promise.then(function(list) {
-      vm.options = filterCaseInsensitiveDuplicates(list);
-    });
-        
+    vm.options = ServerStudy.query({ distinct: vm.property });
+    
     function cancel() {
       $mdDialog.cancel();
     }
@@ -22,22 +20,7 @@
     function submit() {
       $mdDialog.hide(vm.field);
     }
-    
-    // http://stackoverflow.com/questions/26221730/prefer-uppercase-unique-when-doing-case-insensitive-sort-removing-duplicates-ins
-    function filterCaseInsensitiveDuplicates(list) {
-      var sortedList = list.sort(function(a, b) {
-        return a.localeCompare(b, { sensitivity: 'base' });
-      });
-      var i = sortedList.length;
-      var filteredList = (i >= 1 ? [sortedList[i - 1]] : []);
-      while (i--) {
-        if (filteredList[0].toLowerCase() != sortedList[i].toLowerCase()) {
-          filteredList.unshift(sortedList[i]);
-        }
-      }
-      return filteredList;
-    }
-    
+        
   }
 
 })();
