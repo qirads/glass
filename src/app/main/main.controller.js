27719f469b12c51a6dbd02fad3c20dc4c3b5c024
yaacovCR <yaacovCR@gmail.com>
2017-12-11 20:40:31 -0500

    [fix] mobile ui
    hide side-nav

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -6,13 +6,18 @@
     .controller('MainController', MainController);
 
   /** @ngInject */
-  function MainController($state, auth) {
+  function MainController($mdSidenav, auth) {
     var vm = this;
     //vm.creationDate = 1449374294108;
+    vm.toggleSidenav = toggleSideNav;    
     vm.logout = auth.logout;
     vm.isLoggedIn = auth.isLoggedIn;
     vm.loggedInUserName = auth.loggedInUserName;
     vm.hasAdminPrivileges = auth.hasAdminPrivileges;
+
+    function toggleSideNav() {
+      $mdSidenav('left').toggle();
+    }
   }
     
 })();
